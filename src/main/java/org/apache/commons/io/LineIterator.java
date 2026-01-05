0c99881a23a861f527d3b475174bea153bb82c1e
Codacy Archive <codacy-archive@local>
2026-01-05 12:12:43 +0000

    snapshot 2026-01-05T12:12:43.111499+00:00

--- src/main/java/org/apache/commons/io/LineIterator.java
+++ src/main/java/org/apache/commons/io/LineIterator.java
@@ -56,9 +56,9 @@ public class LineIterator implements Iterator<String>, Closeable {
      * Closes a {@link LineIterator} quietly.
      *
      * @param iterator The iterator to close, or {@code null}.
+     * @see Throwable#addSuppressed(Throwable)
      * @deprecated As of 2.6 deprecated without replacement. Please use the try-with-resources statement or handle
      * suppressed exceptions manually.
-     * @see Throwable#addSuppressed(Throwable)
      */
     @Deprecated
     public static void closeQuietly(final LineIterator iterator) {
@@ -142,6 +142,7 @@ public class LineIterator implements Iterator<String>, Closeable {
     /**
      * Overridable method to validate each line that is returned.
      * This implementation always returns true.
+     *
      * @param line  the line that is to be validated.
      * @return true if valid, false to remove from the iterator.
      */
